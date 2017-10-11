02002e330b8d25e9438fca9b21625f6145f54ee0
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-10-11 15:47:05 +0100

    Code refactoring

--- bank_enumeration_lab/bank_enumeration_lab_start_code/bank.js
+++ bank_enumeration_lab/bank_enumeration_lab_start_code/bank.js
@@ -11,10 +11,15 @@ Bank.prototype = {
 }
 
 Bank.prototype.totalValue = function(){
-  var total = 0;
-  this.accounts.forEach(function(account){
-    total += account.value;
-  })
+  // var total = 0;
+  // this.accounts.forEach(function(account){
+  //   total += account.value;
+  // })
+  // return total;
+
+  var total = this.accounts.reduce(function(runningTotal, currentAccount) {
+    return runningTotal + currentAccount.value;
+  }, 0);
   return total;
 }
 
@@ -28,15 +33,21 @@ Bank.prototype.accountByName = function(name){
   }
 
   Bank.prototype.largestAccount = function () {
-    var highestValue = 0;
-    var highestAccount = this.accounts[0];
-    this.accounts.forEach(function(account){
-      if(account.value > highestValue) {
-        highestValue = account.value;
-        highestAccount = account;
-      }
+    var newArray = this.accounts.sort(function(a, b) {
+      return b.value - a.value;
     })
-      return highestAccount;
+
+    return newArray[0];
+
+    // var highestValue = 0;
+    // var highestAccount = this.accounts[0];
+    // this.accounts.forEach(function(account){
+    //   if(account.value > highestValue) {
+    //     highestValue = account.value;
+    //     highestAccount = account;
+    //   }
+    // })
+    //   return highestAccount;
   };
 
 Bank.prototype.averageValue = function () {
